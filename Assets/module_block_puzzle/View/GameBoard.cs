a7db67684738d7e8bda847d40a15b2ad5be00b60
quocduongg <pqduong2015@gmail.com>
2023-01-10 20:25:57 +0700

    ok

--- Assets/module_block_puzzle/View/GameBoard.cs
+++ Assets/module_block_puzzle/View/GameBoard.cs
@@ -1702,6 +1702,12 @@ namespace BlockPuzzle
                         var currencyLog = new TradeLog((int) LogItemType.Collecting, "star");
                         var trade = new Trade<Product,Product>(price,currentGameSetting.boxRewards[product.parameter],(int)LogPlacement.GamePlay,currencyLog,null);
                         var input = new ShowRewardInput(trade,rewardTypeFlag,1,product.parameter,2f);
+                        input.logInterstitial = new SonatLogShowInterstitial()
+                        {
+                            placement = "classic_claim_progress",
+                            level = PlayerData.playTimes,
+                            mode = "classic"
+                        };
                         //parameter = 1, // 0 = no tween box, 1 2 3 = tween box
                         // parameter2 = product.parameter, // index of box
                         RootView.rootView.screenRoot.ShowReward(input);
